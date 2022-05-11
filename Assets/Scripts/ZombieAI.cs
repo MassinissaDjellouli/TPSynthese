fa718895fd8d146b31511dbcb97557be571a28a9
q1w2e3hhhh5 <salahedineraquib@gmail.com>
2022-05-11 16:39:28 -0400

    zombie can now rotate and chase after the player

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -8,9 +8,9 @@ public class ZombieAI : MonoBehaviour
     private Animator anim;
     private GameObject player = null;
 
-    public GameObject Player;
     public int zombieHp = 6;
     public float movementSpeed = 10f;
+    public float speed = 1.0f;
 
     void Start()
     {
@@ -21,41 +21,30 @@ public class ZombieAI : MonoBehaviour
 
     void Update()
     {
-        //if player is alive then run towards him
-        //if player is dead then idle
-
-            anim.SetBool("isRunning", true);
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
 
 
-        //Zombie runs towards player
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            anim.SetBool("isRunning", true);
-        }
-        //when the zombie is close enough, the zombie kills the player
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            anim.SetBool("Attack", true);
-        }          
+        Vector3 targetDirection = player.transform.position - transform.position;
+        float singleStep = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
 
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            stopAllAnimation();
-        }
 
-        //Zombie dies, zombieHp < 0
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            anim.SetBool("isDead", true);
-        }
-
+        anim.SetBool("isRunning", true);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
 
         //TEMPORARY cant make the colliders work so im using this instead
         if (transform.position == player.transform.position)
         {
-            Debug.Log("Impact");
-            stopAllAnimation();
-            anim.SetBool("Attack", true);
+           // Debug.Log("Impact");
+           // stopAllAnimation();
+           // anim.SetBool("Attack", true);
 
+        }
+        else
+        {
+          //  Debug.Log("runnin runnin runnin");
+          //  anim.SetBool("isRunning", true);
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed * Time.deltaTime);
         }
 
 
@@ -69,11 +58,6 @@ public class ZombieAI : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-     //   if (other.CompareTag("Player")) {
-      //      Debug.Log("Impact");
-      //      anim.SetBool("isRunning", false);
-      //     anim.SetBool("Attack", true);
-       // }
 
     }
 
